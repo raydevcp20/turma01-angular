8180073e53c535b06431217ac2c75f8e63f13abd
raydevcp20 <rayanecristina.19@gmail.com>
2025-03-07 18:32:29 -0300

    adicionando o pacote de idioma para o português usado na aula 03 e 04

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import localePt from '@angular/common/locales/pt';
+import localeEn from '@angular/common/locales/en';
+import localePtExtra from '@angular/common/locales/extra/pt';
+import localeEnExtra from '@angular/common/locales/extra/en';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt, 'PT', localePtExtra); // registrando o sufixo 'PT' como português
+registerLocaleData(localeEn, 'En', localeEnExtra); // registrando o sufixo 'En' como inglês
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes)
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'PT' }, // Adicionando o LOCALE_ID como PT
+    { provide: LOCALE_ID, useValue: 'En' } // Adicionando o LOCALE_ID como En
   ]
 };
